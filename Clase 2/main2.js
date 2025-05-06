a8be189436aeed96542ef881cfe02cad75dae1fd
Giancarla Hinojosa <d4l1463194@gmail.com>
2025-05-06 18:00:48 -0300

    Desafio3

--- Clase 2/main2.js
+++ Clase 2/main2.js
@@ -1,7 +1,14 @@
 
-let prod= "Celular"
-let precio=205555
-let cant=2
-
+let prod= prompt("Ingrese el nombre del producto")
+let precio=prompt("Ingrese el precio del producto")
+let cant=prompt("Ingrese la cantidad del producto que va comprar")
 let precioFinal= precio * cant
-console.log (`el precio final es $ ${precioFinal}`)
+if (cant>=5){
+    alert(`
+        Se le aplica el descuento del 10% al producto: ${prod}
+        Su precio original es de $ ${precioFinal}
+        pero aplicado el descuento es de $ ${precioFinal - (precioFinal * 0.1)}
+        `)
+    precioFinal=precioFinal-(precioFinal*0.1)
+}
+alert(`el precio final es de $ ${precioFinal}`)
