d4e19038222608f87a8a84c059e3b9d9ed4179e8
Giancarla Hinojosa <d4l1463194@gmail.com>
2025-05-13 13:45:05 -0300

    prueba

--- DesafiosDW2/main2.js
+++ DesafiosDW2/main2.js
@@ -1,18 +1,4 @@
 
-//___________DESAFIO 3___________
-let prod= prompt("Ingrese el nombre del producto")
-let precio=prompt("Ingrese el precio del producto")
-let cant=prompt("Ingrese la cantidad del producto que va comprar")
-let precioFinal= precio * cant
-if (cant>=5){
-    alert(`
-        Se le aplica el descuento del 10% al producto: ${prod}
-        Su precio original es de $ ${precioFinal}
-        pero aplicado el descuento es de $ ${precioFinal - (precioFinal * 0.1)}
-        `)
-    precioFinal=precioFinal-(precioFinal*0.1)
-}
-alert(`el precio final es de $ ${precioFinal}`)
 
 
 //___________DESAFIO 4___________
